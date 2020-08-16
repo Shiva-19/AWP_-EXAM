cab684508fad86bbb5b8a6ae9e606fc72b71187c
Shiva-19 <65189006+Shiva-19@users.noreply.github.com>
2020-08-16 16:20:32 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,18 @@
+function Form() {
+    let newel = document.createElement("div");
+
+    let a = document.querySelector("#input1").value;
+    let c = document.querySelector("#input2").value;
+
+    newel.appendChild(a);
+    newel.appendChild(b);
+
+}
+
+
+
+
+
+
+
+
